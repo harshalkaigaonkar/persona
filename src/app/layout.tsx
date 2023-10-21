77ae026300e6b03dc5232020ad2daa4f10acd6a6
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2023-10-21 13:03:09 +0530

    added vercel analytics support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import data from 'config';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
       <head>
         <link rel="icon" href={data.profile_img}></link>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
