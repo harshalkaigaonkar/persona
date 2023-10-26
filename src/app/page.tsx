67046c2867efb2c5ffb6ae94c0ed794a4ae88640
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2023-10-27 03:41:17 +0530

    add: animation on scroll for visiblity 0 for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,32 @@ import data from 'config';
 import Image from 'next/image';
 import { UFrameType, UTileType } from './types';
 import { uuid } from 'uuidv4';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ListTile } from 'components/list-tile';
 import ProfileImage from '../../public/Harshal.jpg';
 import { Transition } from '@headlessui/react';
 
 export default function Home() {
+  const tilesRef = useRef<HTMLDivElement | null>(null);
+  const headerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const onScroll: EventListener = (event: Event) => {
+      const scrollOffset = tilesRef && tilesRef.current.getBoundingClientRect();
+      if (!!headerRef.current) {
+        const opacityValue = scrollOffset.top / tilesRef.current.offsetTop;
+        headerRef.current.style.opacity = (
+          opacityValue > 0 ? opacityValue : 0
+        ).toString();
+      }
+    };
+
+    const win: Window = window;
+    win.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   function tileDistribution(tileType: any, data: any) {
     return tileType === UTileType.MAINTILE ? (
       <>
@@ -137,7 +157,10 @@ export default function Home() {
               enterFrom="opacity-0 top-8"
               enterTo="opacity-100 top-0"
             >
-              <div className="pt-10 sticky top-0 w-full h-30 flex justify-center items-center">
+              <div
+                ref={headerRef}
+                className="pt-10 sticky top-0 w-full h-30 flex justify-center items-center"
+              >
                 <div className=" w-full h-full flex flex-col justify-center items-center ">
                   <div className="flex justify-center items-center w-28 h-28 rounded-xl">
                     <Image
@@ -166,8 +189,11 @@ export default function Home() {
               enterFrom="opacity-0 mt-10"
               enterTo="opacity-100 mt-2"
             >
-              <div className="w-full h-full flex justify-center items-center">
-                <div className="mt-2 p-1 lg:p-10 md:p-8 w-full max-w-[90%] md:max-w-[80%] h-full flex flex-col md:flex-row flex-wrap justify-evenly items-start gap-4">
+              <div
+                ref={tilesRef}
+                className="w-full h-full flex justify-center items-center"
+              >
+                <div className="mt-2 p-1 lg:p-10 md:p-8 w-full max-w-[90%] md:max-w-[80%] h-full flex flex-col md:flex-row flex-wrap justify-evenly items-start gap-4 scroll-smooth">
                   {data.frames.map(
                     ({ frameType, data }: any, index: number) => (
                       <React.Fragment key={index}>
